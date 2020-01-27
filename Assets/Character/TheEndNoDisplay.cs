d220a54f568866f06965fe9fdafdbcdb9c936886
unknown <clelia.pelleteret@gmail.com>
2020-01-27 09:29:21 +0100

    Add new PNJ with The ENd Interaction

---
+++ Assets/Character/TheEndNoDisplay.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TheEndNoDisplay : MonoBehaviour
+{
+	public Image image;
+	public Text text;
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Turns the image off.
+        image.enabled = false;
+        text.enabled = false;
+    }
+}
---
+++ Assets/Dialogs/DisplayTheEND.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayTheEND : MonoBehaviour
+{
+	public Button myButton;
+    public Image image;
+    public Text text;
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+    	Button btn = myButton.GetComponent<Button>();
+    	btn.onClick.AddListener(displayTheEnd);
+    }
+
+    void displayTheEnd() {
+        image.enabled = !image.enabled;
+        text.enabled = !text.enabled;
+    }
+
+}
